81597058a52b6548d07bc92ba16c836e2193f651
badri7489 <badrisharma333@gmail.com>
2023-03-06 11:59:54 +0530

    Initial connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 // routes
@@ -6,6 +7,13 @@ app.get('/', (req, res) => {
 	res.send('Hello from API!!');
 });
 
-app.listen(6789, () => {
-	console.log('Node JS is running on port 6969');
+mongoose.connect(
+	'mongodb+srv://admin:badri6291049175@badriapi.b7boyev.mongodb.net/Node-API?retryWrites=true&w=majority'
+).then(() => {
+    console.log("Hello niggas!!! I connected to your mongodb.");
+    app.listen(6789, () => {
+		console.log('Node JS is running on port 6969');
+	});
+}).catch((error) => {
+    console.log(error);
 });
