090b0051db7c316ddd4c542c8bfb33fd40b6877c
badri7489 <badrisharma333@gmail.com>
2023-03-06 12:33:14 +0530

    Model and post data

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModel');
 const app = express();
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/', (req, res) => {
 	res.send('Hello from API!!');
 });
 
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.send(500).json({ message: error.message });
+    }
+})
+
 mongoose.connect(
 	'mongodb+srv://admin:badri6291049175@badriapi.b7boyev.mongodb.net/Node-API?retryWrites=true&w=majority'
 ).then(() => {
     console.log("Hello niggas!!! I connected to your mongodb.");
-    app.listen(6789, () => {
+    app.listen(3000, () => {
 		console.log('Node JS is running on port 6969');
 	});
 }).catch((error) => {
